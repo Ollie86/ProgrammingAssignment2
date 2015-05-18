6c35c182c96e54c5f5c7fc1c6facc5145365be07
Timo Olsthoorn <timo.olsthoorn@incentro.com>
2015-05-18 20:45:08 +0200

    Implementation of the Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This program is written to cache the results of an matrix inverse
+## calculation. The "cacheSolve" function is passed a "makeCacheMatrix" 
+## function closure which provides a caching mechanism.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## The makeCacheMatrix closure is used to store the matrix and possibly
+## the inverse of the matrix. To retrieve or set the (inverse)matrix
+## if provides the following functions
+##  - set(new_matrix)
+##  - get() : returns matrix
+##  - setInverse(inverse)
+##  - getInverse()  : returns inverse_matrix
+##
+## Note: The inverse of the matrix is not calculated in this closure!
+makeCacheMatrix <- function(matrix = matrix()) {
+    inverse_matrix <- NULL
+    set <- function(new_matrix) {
+        matrix <<- new_matrix
+        inverse_matrix <<- NULL
+    }
+    get <- function() {
+        matrix
+    }
+    setInverse <- function(inverse) {
+        inverse_matrix <<- inverse
+    }
+    getInverse <- function() {
+        inverse_matrix
+    }
+    list(set = set, 
+         get = get, 
+         setInverse = setInverse, 
+         getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
+## cacheSolve is passed an makeCacheMatrix instance as x
+## x is then used to retrieve the inverse of the matrix 
+## if no inverse was returned by the cache the inverse is 
+## calculated and then set in the cache
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inverse <- x$getInverse()
+        if(is.null(inverse)) {
+            # Only calculate the inverse if the cache 
+            # didn't contain the inverse
+            inverse <- solve(x$get)
+            x$setInverse(inverse)
+        } 
+        inverse
 }
